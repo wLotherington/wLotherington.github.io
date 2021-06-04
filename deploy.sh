e183d0613c2639519eba69a03f91ff86de8f1ef8
Will Lotherington <will.lotherington@hey.com>
2021-06-04 11:09:37 -0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,23 @@ set -e
 # build
 npm run build
 
+git checkout ghpages
+mv 2020 dist
+
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
-echo 'www.ekko-realtime.com' > CNAME
+# echo 'www.ekko-realtime.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:wLotherington/wLotherington.github.io.git main
+# git push -f git@github.com:wLotherington/wLotherington.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
